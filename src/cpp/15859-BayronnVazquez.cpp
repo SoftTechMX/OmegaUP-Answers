5dca2e15ab09a4e5d5530b612089ecc9c4d31fc2
Bayron Vazquez <bayronnjonathan@gmail.com>
2023-01-12 12:01:15 -0600

    Solucion al problema https://omegaup.com/arena/problem/A-5-Session4/#problems

---
+++ src/cpp/15859-BayronnVazquez.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int mes;
+    cin >> mes;
+    switch (mes) {
+    case 1:
+    cout << "Enero";
+        break;
+    case 2:
+    cout << "Febrero";
+        break;
+    case 3:
+    cout << "Marzo";
+        break;
+    case 4:
+    cout << "Abril";
+        break;
+    case 5:
+    cout << "Mayo";
+        break;
+    case 6:
+    cout << "Junio";
+        break;
+    case 7:
+    cout << "Julio";
+        break;
+    case 8:
+    cout << "Agosto";
+        break;
+    case 9:
+    cout << "Septiembre";
+        break;
+    case 10:
+    cout << "Octubre";
+        break;
+    case 11:
+    cout << "Noviembre";
+        break;
+    case 12:
+    cout << "Diciembre";
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
