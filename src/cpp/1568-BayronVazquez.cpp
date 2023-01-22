5772d919a21be56abb9c9922a82880737dc761c2
Bayron Vazquez <bayronnjonathan@gmail.com>
2023-01-21 18:29:39 -0600

    solucion al problema https://omegaup.com/arena/problem/ptwo/

---
+++ src/cpp/1568-BayronVazquez.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << fixed << setprecision(0) << pow(2,n) << endl;
+    return 0;
+}
